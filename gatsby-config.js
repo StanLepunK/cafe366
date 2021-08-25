a683005b60729fe00fb11fa2dc4bd57dde73d7ba
Stan le Punk <stanislasiii@gmail.com>
2021-08-25 11:36:40 +0200

    test to deploy on gatsby cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,15 +2,20 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://cafe-366.myshopify.com`,
   },
-  plugins: [
-    {
-      resolve: `gatsby-source-shopify`,
-      options: {
-        apiKey: process.env.SHOPIFY_API_KEY,
-        password: process.env.SHOPIFY_SHOP_PASSWORD,
-        storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
-        shopifyConnections: ["collections"],
-      },
-    },
-  ],
+
+  flags: {
+    FAST_DEV: true,
+  },
+  // plugins: [
+  //   {
+  //     resolve: `gatsby-source-shopify`,
+  //     options: {
+  //       apiKey: process.env.SHOPIFY_API_KEY,
+  //       password: process.env.SHOPIFY_SHOP_PASSWORD,
+  //       storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+  //       // storeUrl: "cafe-366.myshopify.com",
+  //       shopifyConnections: ["collections"],
+  //     },
+  //   },
+  // ],
 };
